30e3e1932fd06f902a7cc64e01f0fc6de32226e1
sspon001 <sspon001@ucr.edu>
2021-11-06 15:28:43 -0700

    ex1 done

---
+++ Lab6/header/timer.h
@@ -0,0 +1,4 @@
+#ifdef TIMER_H
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#endif
--- Lab6/source/main.c
+++ Lab6/source/main.c
@@ -8,16 +8,76 @@
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum states {start, init, nextLed} state ;
+
+volatile unsigned char TimerFlag = 0;
+void TimerISR() { TimerFlag = 1;}
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+void TimerOn() {
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+void TimerOff() {
+	TCCR1B = 0x00;
+}
+ISR(TIMER1_COMPA_vect) {
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0) {
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+void TimerSet (unsigned long M) {
+	_avr_timer_M = M;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+void Tick(){
+	switch(state){
+		case start:
+			state = init ;
+			break ;
+		case init:
+			state = nextLed ;
+			break ;
+		case nextLed:
+			state = nextLed ;
+			break ;
+		default:
+			state = start ;
+			break ;
+	}
+	switch(state){
+		case start:
+			break ;
+		case init:
+			PORTB = 0x01 ;
+			break ;
+		case nextLed:
+			PORTB = (PORTB == 0x04) ? 0x01 : PORTB << 1 ;
+			break ;
+		default: break ;
+	}
+}
+
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
+	DDRB = 0xFF ; PORTB = 0x00 ;
+	TimerSet(1000) ;
+	TimerOn() ;
     while (1) {
-
+	    Tick() ;
+	    while(!TimerFlag) {} ;
+	    TimerFlag = 0 ;
     }
-    return 1;
+    return 1 ;
 }
